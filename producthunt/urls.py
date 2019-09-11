e223677e2ef8dc9168901fb173b22ab4ff3381b6
和田翔大 <kakehiro@wadashoudainoMacBook-Pro.local>
2019-09-11 19:28:18 +0900

    Maybe complete version

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -3,12 +3,12 @@ from django.urls import path, include
 from products import views
 import products
 from accounts import views, urls
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', products.views.home, name='home'),
     path('accounts/', include('accounts.urls')),
-    #path('accounts/login', include('accounts.urls.login')),
-    #path('accounts/logout', include('accounts.urls.logout')),
-
-]
+    path('products/', include('products.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Product class
+class Product(models.Model):
+# title
+    title = models.CharField(max_length=30)
+# url
+    url = models.URLField(max_length=200)
+# pub_date
+    pub_date = models.DateTimeField()
+# votes_total
+    votes_total = models.IntegerField(default=1)
+# image
+    image = models.ImageField(upload_to='images/',height_field=None, width_field=None)
+# icon
+    icon = models.ImageField(upload_to='images/',height_field=None, width_field=None)
+# body
+    body = models.TextField(max_length=1000)
+
+# pub_date_pretty
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+
+    def __str__(self):
+        return self.title
+
+
+    def summary(self):
+        return self.body[:100]
+
+# hunter
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
